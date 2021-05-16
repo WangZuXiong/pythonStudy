0a4b81801b58bf17c7b912972643b60b0dafc760
wangzuxiong <zuxiongwang@163.com>
2021-05-16 10:47:18 +0800

    python 常见api

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 
 # 按 ⌃R 执行或将其替换为您的代码。
 # 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
+import time
+
 import pyCoding
 import pyDataStructure
 import pyDictionary
 import pyErrorExecptions
+import pyFast
 import pyFunction
 import pyInputOutput
 import pyIter
@@ -27,6 +30,7 @@ def print_hi(name):
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
+    start_time = time.time()
     pass
     # pyNumber.foo()
     # pyString.foo()
@@ -75,6 +79,18 @@ if __name__ == '__main__':
     # pyInputOutput.inoput_test()
     # pyInputOutput.write_file()
     # pyInputOutput.pickle_test()
-    pyErrorExecptions.foo()
+    # pyErrorExecptions.foo()
+    # pyFast.zip_test()
+    # pyFast.str_combin()
+    # pyFast.re_find_all()
+
+    # pyFast.max_test()
+    # pyFast.deepcopy_test()
+    pyFast.isinstance_test()
+
+
+
+    end_time = time.time()
+    print("======>Time:{0}".format(end_time - start_time))
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
--- pyFast.py
+++ pyFast.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from copy import deepcopy
+import re
 
 
 # 字符元素组成判定  检查两个字符串的组成元素是不是一样的
@@ -31,3 +33,71 @@ def split_list(raw_list, count):
             group_index += 1
         result[group_index].append(data)
     return result
+
+
+def zip_test():
+    lst = [['a', 'b'], ['c', 'd'], ['e', 'f']]  # [[1, "a"], [2, "b"], [3, "c"]]
+    transposed = zip(*lst)
+
+    for item in transposed:  # 是一个迭代器对象
+        print(item)
+
+    print(list(transposed))  # [('a', 'c', 'e'), ('b', 'd', 'f')]
+
+    print(range(3))  # range(0, 3)
+    print(range(4))
+    print(list(zip(range(3), range(4))))  # [(0, 0), (1, 1), (2, 2)]
+    # 将n序列
+
+    key = ["x", "y"]
+    value = [100, 300]
+    temp_dict = dict(zip(key, value))
+    print(temp_dict)
+
+
+def str_combin():
+    int_lst = [1, 2, 3, 4]
+    # print(",".join(int_lst))#TypeError: sequence item 0: expected str instance, int found
+
+    str_lst = ["1", "2", "3"]
+    print(",".join(str_lst))  # 1,2,3
+
+
+def re_find_all():
+    t = re.findall(r'[abc]', "1A2a1啊2abc", re.IGNORECASE)  # 没有区分大小写
+    print(t)  # ['A', 'a', 'a', 'b', 'c']
+
+
+def max_test():
+    lst = [1, 1, 1, 2, 3, 4]
+    # list 里面出现频率最多的元素
+    t = max(set(lst), key=lst.count)
+    print(t)  # 1
+
+
+def deepcopy_test():
+    lst = [1, 2, 3]
+    lst1 = lst
+    lst2 = deepcopy(lst)
+
+    lst[0] = 100
+    print(lst1)  # [100, 2, 3]
+    print(lst2)  # [1, 2, 3]
+
+    lst = [9, 9, 9]
+    print(lst1)  # [100, 2, 3]
+    print(lst2)  # [1, 2, 3]
+
+    lst1 = [6, 6, 6]
+    print(lst)  # [9, 9, 9]
+    print(lst2)  # [1, 2, 3]
+
+
+def isinstance_test():
+    lst = [(1, 2, 3), [4, 5, 6]]
+    result = []
+    for t in lst:
+        if isinstance(t, list):
+            result.extend(t)
+
+    print(result)  # [4, 5, 6]  tuple is not list instance
